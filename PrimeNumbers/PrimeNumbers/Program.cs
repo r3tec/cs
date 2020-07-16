6cb7bf86c58cfb8a5bc02cd7b56a1dea23272922
alex <alex@r3-tec.com>
2020-07-15 20:10:42 -0400

    Boilerplage for IsPrime

--- PrimeNumbers/PrimeNumbers/Program.cs
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -6,8 +6,21 @@ namespace PrimeNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Checking if number is prime");
+            int num = 17;
+            
+            if (IsPrime(num)) Console.WriteLine("It is Prime!!");
+            else Console.WriteLine("It is not Prime!!");
+
+            num = 12;
+            if (IsPrime(num)) Console.WriteLine("It is Prime!!");
+            else Console.WriteLine("It is not Prime!!");
+        }
+
+        private static bool IsPrime(int numberToTest)
+        {
+            throw new NotImplementedException();
+            // Remove first line and put your code here
         }
     }
 }
- 
