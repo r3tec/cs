93d3a104030fe6f95457a838a7275f1aa320016d
DimitryStarski <dimitry.starski@users.noreply.github.com>
2020-07-16 13:26:13 -0400

    [ADDED] IsPrime method

--- PrimeNumbers/PrimeNumbers/Program.cs
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -17,10 +17,16 @@ namespace PrimeNumbers
             else Console.WriteLine("It is not Prime!!");
         }
 
-        private static bool IsPrime(int numberToTest)
+        private static bool IsPrime(int n)
         {
-            throw new NotImplementedException();
-            // Remove first line and put your code here
+            if (n <= 1) 
+           return false;   
+    
+            for (int i = 2; i < n; i++) 
+                if (n % i == 0) 
+                return false; 
+  
+            return true; 
         }
     }
 }
